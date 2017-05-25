d955962e5c0c8e27f87af95c6c2d5d08269039ec
sbartkiv <sergiibartkiv@gmail.com>
2017-05-25 14:57:23 +0300

    add adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,44 +1,43 @@
 (function() {
 
-    var $vividBlock = $(".vivid"), // scrollable block
+    var vividBlock = $(".vivid"), // scrollable block
         imgWidth = $(".before-after__pic").width(), // main picture
         initSplit = Math.round(imgWidth / 2);
 
-    $vividBlock.width(initSplit);
+    vividBlock.width(initSplit);
 
     $(".before-after__window").mousemove(function(e) {
-
-        var offX = e.offsetX || e.clientX - $vividBlock.offset().left;
-        $vividBlock.width(offX);
-      
+        var offX = e.offsetX || e.clientX - vividBlock.offset().left;
+        vividBlock.width(offX);
     });
 
     $(".before-after__window").mouseleave(function(e) {
-
-        $vividBlock.stop().animate(
+        vividBlock.stop().animate(
           {
               width: initSplit
           },
           1000
         );
-
     });
 
     $('.slider__item').click(function(e){
-
         $('.slider__item').removeClass("active");
         $(this).addClass("active");
 
         var content = $(this).html();
-
         $('.before-after__img-out').html(content);
-
     });
 
     $('input[type=range]').on("change mousemove", function() {
-
         $('.vivid').css("background-color", "rgba(255,255,255," + $(this).val() + ")");
+    });
 
+    $('.menu-toggle').click(function(){
+      $(".mobile-menu").toggle();
+    });
+
+    $('.mobile-menu__close').click(function(){
+      $(".mobile-menu").hide();
     });
 
 })();
